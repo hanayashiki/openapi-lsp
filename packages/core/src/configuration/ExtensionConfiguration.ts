3d6931bc8d54d19860270f2597500fec020abc08
hanayashiki <cwang592@usc.edu>
2026-01-06 01:05:15 +0900

    fix: typo in configuration description

--- packages/core/src/configuration/ExtensionConfiguration.ts
+++ packages/core/src/configuration/ExtensionConfiguration.ts
@@ -4,7 +4,7 @@ export const ExtensionConfiguration = z.object({
   "openapi-lsp.discoverRoots.pattern": z
     .string()
     .describe(
-      `The minimatch (https://www.npmjs.com/package/minimatch) glob pattern to ignore during OpenAPI document discovery`
+      `The minimatch (https://www.npmjs.com/package/minimatch) glob pattern for OpenAPI document discovery`
     )
     .default("**/*"),
   "openapi-lsp.discoverRoots.ignore": z
